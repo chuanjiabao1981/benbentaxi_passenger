bd5422aa79d9c26318e73441c261a689e5cca325
guowei <chuanjiabao1981@gmail.com>
2013-08-03 12:30:06 +0800

    remote exception

--- src/com/benbentaxi/remoteexception/RemoteExceptionTask.java
+++ src/com/benbentaxi/remoteexception/RemoteExceptionTask.java
@@ -1,31 +1,21 @@
 package com.benbentaxi.remoteexception;
 
-import org.json.JSONObject;
 
 import android.util.Log;
 
 import com.benbentaxi.Configure;
-import com.benbentaxi.Session;
-import com.benbentaxi.api.JsonHttpRequest;
 import com.benbentaxi.api.PostTask;
-import com.benbentaxi.passenger.location.DemoApplication;
-import com.benbentaxi.passenger.taxirequest.TaxiRequest;
-import com.benbentaxi.passenger.taxirequest.create.CreateTaxiRequestFormResponse;
 
 public class RemoteExceptionTask extends PostTask{
 	private final static String API						=	"/api/v1/client_exceptions";
 	private final String TAG			     			= RemoteExceptionTask.class.getName();
 	private Configure       mConfigure					=  null;
-	private Session mSession 							= null;
-	private DemoApplication mApp 						= null;
-	private JsonHttpRequest mJsonHttpRequest			= null;
 	private RemoteExceptionRequest	mExceptionRequest		= null;
 	
 	public RemoteExceptionTask(String exception)
 	{
 		mExceptionRequest		= new RemoteExceptionRequest(exception);
 		mConfigure				= new Configure();
-		mJsonHttpRequest		= new JsonHttpRequest();
 	}
 	protected String getPostParams() {
 		String str = mExceptionRequest.toJson().toString();
